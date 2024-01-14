7c9c9862cddc5a482d3708d8b50a8fb90078670e
Josefina Bozzola <bozzolajosefina@gmail.com>
2024-01-14 18:24:26 -0300

    Parte_1b

--- Proyecto/Proyecto/settings.py
+++ Proyecto/Proyecto/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-flbkh4j&02p_i9=6+f)qo5e*)ggho!5a_62e%#bkd96=4f+d-b'
-
+from django.core.management.utils import get_random_secret_key
+#SECRET_KEY = 'django-insecure-flbkh4j&02p_i9=6+f)qo5e*)ggho!5a_62e%#bkd96=4f+d-b'
+SECRET_KEY = get_random_secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
